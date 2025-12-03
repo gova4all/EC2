199e89e9e30bbf0436e0103cb9207d8145a84a60
gova4all <agovardhannaidu@gmail.com>
2025-12-03 03:04:32 -0500

    few changes

--- variables.tf
+++ variables.tf
@@ -6,10 +6,7 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "key_name" {
-  description = "Existing EC2 key pair"
-  type        = string
-}
+
 
 variable "instance_name" {
   default = "Jenkins-TF-EC2"
