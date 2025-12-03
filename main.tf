968051b169e22ab69ef7c2117f1d624a53ae23f6
gova4all <agovardhannaidu@gmail.com>
2025-12-03 02:53:59 -0500

    few changes Ec2 Module

--- main.tf
+++ main.tf
@@ -1,16 +1,45 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 5.6"       # current stable version
-
-  name = var.instance_name
-
-  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  monitoring             = true
-
-  tags = {
-    Project = "Jenkins-Terraform"
-    Owner   = "Govardhan"
+##Get existing VPC by Name tag##
+data "aws_vpc" "existing" {
+  filter {
+    name   = "tag:Name"
+    values = ["My_VPC"]
   }
 }
+##Get all subnets in that VPC##
+data "aws_subnets" "existing" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
+}
+##Get an existing Security Group##
+
+data "aws_subnet" "public_1" {
+  filter {
+    name   = "tag:Name"
+    values = ["Public_Subent_01"]
+  }
+}
+##Get an existing Security Group##
+data "aws_security_group" "default" {
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+
+  vpc_id = data.aws_vpc.existing.id
+}
+
+
+module "ec2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.2.0"
+
+  name          = "my-ec2"
+  instance_type = "t2.micro"
+  ami           = "ami-0c55b159cbfafe1f0"
+
+  subnet_id              = data.aws_subnet.public_1.id
+  vpc_security_group_ids = [data.aws_security_group.default.id]
+}
+
